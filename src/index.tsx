fdce46832b7f3f60503e510711979148a06f50d7
dhashdev <dhruv@dhash.dev>
2022-08-28 13:05:39 +0530

    updated manifest json

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
 import * as servicerWorkerRegisteration from "./serviceWorkerRegistration";
@@ -9,12 +9,12 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 servicerWorkerRegisteration.register();
